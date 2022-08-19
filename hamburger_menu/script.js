7734622cef230d9feef18640c816ea638b1e3a11
masakimasui <m.masui2003@gmail.com>
2022-08-19 13:10:41 +0900

    ハンバーガーメニューの内容を作成しました。JSで動きを実装中です

---
+++ hamburger_menu/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('overlay');
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+  console.log('表示できました')
+}
